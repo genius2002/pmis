c2b496b3e8999143c2afff133e47cff265caa27d
wayen <weston.travolta@hotmail.com>
2019-08-01 18:03:15 +0800

    '11'

---
+++ src/main/java/com/wayen/pmsi/PmsiApplication.java
@@ -0,0 +1,13 @@
+package com.wayen.pmsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmsiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmsiApplication.class, args);
+    }
+
+}
