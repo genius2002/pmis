f76bffa0cd1bd5771a2c474b83cdde2a5732420b
wayen <weston.travolta@hotmail.com>
2019-08-02 09:47:58 +0800

    create employee do

---
+++ com.wayen.pmsi/src/main/java/com/wayen/pmsi/doo/Employee.java
@@ -0,0 +1,33 @@
+package com.wayen.pmsi.doo;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class Employee {
+    /**
+     * 名
+     */
+    private String firstName;
+    private String midName;
+    /**
+     * 姓
+     */
+    private String lastName;
+    /**
+     * 员工ID
+     */
+    private String empId;
+    /**
+     * 生日
+     */
+    private Date birthday;
+    /**
+     * 身份证号
+     */
+    private String idCard;
+    private String telNo1;
+    private String telNo2;
+    private String telNo3;
+}
