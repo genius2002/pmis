2e9eb0dfde6859e01b0675b778f28a2dc5e07824
wayen <weston.travolta@hotmail.com>
2019-08-07 11:07:26 +0800

    add method to StringUtils

--- com.wayen.utils/src/main/java/com/wayen/utils/StringUtils.java
+++ com.wayen.utils/src/main/java/com/wayen/utils/StringUtils.java
@@ -1,11 +1,38 @@
 package com.wayen.utils;
 
 public class StringUtils {
+
+    /**
+     * 判断传入字符串是否为null或长度为0
+     *
+     * 空格算长度
+     *
+     * @param src
+     * @return
+     */
     public static boolean isEmpty(String src){
         if(src==null || src.length()==0){
             return true;
         }else{
             return false;
         }
+    }
+
+    /**
+     * 拼接字符串
+     *
+     * null不处理
+     *
+     * @param params
+     * @return
+     */
+    public static String concat(Object... params){
+        StringBuffer sb = new StringBuffer();
+        for (Object param:params) {
+            if(null!=param){
+                sb.append(param);
+            }
+        }
+        return sb.toString();
     }
 }
